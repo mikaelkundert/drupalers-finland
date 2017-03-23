b1672e5c0a357554989aa87ffc3e01a1aa11abb3
Rajib Paudyal <rajib.mp@gmail.com>
2017-03-23 09:05:21 +0200

    sample template for company listings.

---
+++ components/Company.vue
@@ -0,0 +1,23 @@
+<template>
+  <article class="company">
+    <header class="company-header">
+      Company Name
+    </header>
+    <section class="company-body">
+      <img src="">
+    </section>
+    <footer class="company-footer">
+      Footer
+    </footer>
+  </article>
+</template>
+
+<script>
+ export default {
+  data() {
+    return {
+      allCompanies: YAML.load('../data/companies.yml')
+     }
+   }
+}
+</script>
