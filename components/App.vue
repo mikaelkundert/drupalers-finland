bdd1fb451803fcdbb369620350dc9bbb480f0bb8
Rajib Paudyal <rajib.mp@gmail.com>
2017-03-24 23:52:12 +0200

    search field and layout

--- components/App.vue
+++ components/App.vue
@@ -3,13 +3,34 @@
     <main>
       <v-content>
         <v-container fluid>
-          <companies></companies>
+          <section class="search-section">
+            <v-row>
+              <v-col xs4 v-for="i in 2"></v-col>
+              <v-col xs4>
+                <v-text-field
+                  name="search"
+                  label="Search"
+                  class="input-group--focused pr-5"
+                  prepend-icon="search"
+                  placeholder="search"
+                  single-line
+                  >
+                </v-text-field>
+              </v-col>
+            </v-row>
+          </section>
+          <section class="body-section">
+            <companies></companies>
+          </section>
         </v-container>
       </v-content>
     </main>
     <footers></footers>
   </v-app>
 </template>
+
+
+
 
 <script>
 const YAML = require('yamljs')
--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
   <v-footer>
-    <article>
+    <article class="text-sm-left pl-5">
       Drupal is a <a href="http://drupal.com/trademark" target="_blank" rel="nofollow">registered trademark</a> of <a href="http://buytaert.net" target="_blank" rel="nofollow">Dries Buytaert</a>.
     </article>
-    <article>
+    <article class="text-sm-right pr-5">
       Contribute to add missing company in <a href="http://drupal.com/trademark" target="_blank" rel="nofollow">Github</a>
     </article>
   </v-footer>
