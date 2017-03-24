d00020da336ebe83279b5ee7ef02eab899997634
Rajib Paudyal <rajib.mp@gmail.com>
2017-03-24 22:01:08 +0200

    Footer text updated

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,10 @@
 <template>
   <footer>
-    This is dummy footer text
+    <article>
+      Drupal is a <a href="http://drupal.com/trademark" target="_blank" rel="nofollow">registered trademark</a> of <a href="http://buytaert.net" target="_blank" rel="nofollow">Dries Buytaert</a>.
+    </article>
+    <article>
+      Contribute to add missing company in <a href="http://drupal.com/trademark" target="_blank" rel="nofollow">Github</a>
+    </article>
   </footer>
 </template>
