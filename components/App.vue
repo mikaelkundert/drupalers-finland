d431a37872df0138ba9b4b44519f41cfe5ccafe5
Rajib Paudyal <rajib.mp@gmail.com>
2017-03-27 08:12:41 +0300

    Search capability

--- components/App.vue
+++ components/App.vue
@@ -12,12 +12,42 @@
                 prepend-icon="search"
                 placeholder="search"
                 single-line
+                v-model="searchQuery"
                 >
               </v-text-field>
             </article>
           </section>
           <section class="body-section">
-            <companies></companies>
+            <v-row class="row-body">
+              <v-card class="ma-4 company-card" v-for="company in filterCompanies">
+                <v-card-row class="cyan darken-2">
+                  <v-card-title>
+                    <span class="white--text">{{ company.Name}}</span>
+                    <v-spacer></v-spacer>
+                    <v-menu id="space" bottom left origin="top right" transition="v-scale-transition">
+                      <v-btn icon="icon" slot="activator" class="white--text">
+                        <v-icon>more_vert</v-icon>
+                      </v-btn>
+                      <v-list>
+                        <v-list-item v-for="location in company.Locations">
+                          <v-list-tile>
+                            <v-list-tile-title>{{ location }}</v-list-tile-title>
+                          </v-list-tile>
+                        </v-list-item>
+                      </v-list>
+                    </v-menu>
+                  </v-card-title>
+                </v-card-row>
+                <v-card-row height="15em" :class="company.Background" class="company-logo">
+                  <img :src="company.Logo" :alt="company.Name" :title="company.Name" class="pa-2 company-logo-image">
+                </v-card-row>
+                <v-card-row actions class="blue-grey darken-2 mt-0">
+                  <v-spacer></v-spacer>
+                  <a :href="company.Website" target="_blank" rel="nofollow"><button flat class="white--text">{{ company.Website }}</button></a>
+                  <v-spacer></v-spacer>
+                </v-card-row>
+              </v-card>
+            </v-row>
           </section>
         </v-container>
       </v-content>
@@ -26,23 +56,34 @@
   </v-app>
 </template>
 
-
-
-
 <script>
 import Yaml from 'yamljs'
-import Companies from './Company.vue'
 import Footers from './Footer.vue'
 const YAML = Yaml
 const _ = require('lodash');
 
 export default {
-  components: {
-    Companies,
-    Footers
+  data() {
+    return {
+      allCompanies: _.orderBy(YAML.load('./data/companies.yml'), ['Name'], ['asc']),
+      searchQuery: ''
+     }
   },
-  YAML,
-  _
+  computed: {
+    filterCompanies() {
+       return this.findBy(this.allCompanies, this.searchQuery, 'Name')
+     }
+  },
+  methods: {
+    findBy: function (list, value, column) {
+      return list.filter(function (item) {
+        return item[column].includes(value)
+      })
+    }
+  },
+  components: {
+    Footers
+  }
 }
 </script>
 
