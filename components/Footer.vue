dd05bd693c66e249c1f1b71ff04adb696ef367b2
Rajib Paudyal <rajib.mp@gmail.com>
2017-03-27 08:12:06 +0300

    Made links dynamic

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,11 +2,23 @@
   <v-footer class="secondary white--text">
     <div class="row text-xs-center">
       <article class="flow-text px-2 col xs12 md6">
-        Drupal is a <a href="http://drupal.com/trademark" target="_blank" rel="nofollow">registered trademark</a> of <a href="http://buytaert.net" target="_blank" rel="nofollow">Dries Buytaert</a>.
+        Drupal is a <a :href="drupalTrademark" target="_blank" rel="nofollow">registered trademark</a> of <a :href="dries" target="_blank" rel="nofollow">Dries Buytaert</a>.
       </article>
       <article class="flow-text px-2 col xs12 md6">
-        Contribute to add missing company in <a href="https://github.com/rajibmp/drupalers-finland" target="_blank" rel="nofollow">Github</a>
+        Help me add missing companies from the list in <a :href="github" target="_blank" rel="nofollow">Github</a>
       </article>
     </div>
   </v-footer>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      drupalTrademark: 'http://drupal.com/trademark',
+      dries: 'http://buytaert.net',
+      github: 'https://github.com/rajibmp/drupalers-finland'
+     }
+  }
+}
+</script>
